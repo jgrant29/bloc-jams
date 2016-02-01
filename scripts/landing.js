155aec54f61b97d44cdededceea4a6f25b370fce
Justin Grant <jmgrant702@gmail.com>
2016-01-31 20:20:38 -0700

    dom scripting events assignment

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,14 +1,14 @@
+
 var pointsArray = document.getElementsByClassName('point');
 
-    var animatePoints = function(points) {
+    function animatePoints(points) {
  
-        var points = document.getElementsByClassName('point');
-            for(var i = 0; i < points.length; i++) {
-                points[i].style.opacity = 1;
-                points[i].style.transform = "scaleX(1) translateY(0)";
-                points[i].style.msTransform = "scaleX(1) translateY(0)";
-                points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-            }
+        Array.prototype.forEach.call( document.getElementsByClassName('point'), function(points) {
+                points.style.opacity = 1;
+                points.style.transform = "scaleX(1) translateY(0)";
+                points.style.msTransform = "scaleX(1) translateY(0)";
+                points.style.WebkitTransform = "scaleX(1) translateY(0)";
+            })
         };
 
         window.onload = function() {
